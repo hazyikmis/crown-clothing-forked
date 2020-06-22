4f5ae6daa09cf8a5c0cced9636f8249a971b0973
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-06-23 01:04:03 +0200

    email sign in process done using with sagas

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -5,7 +5,12 @@ import {
   googleProvider,
   createUserProfileDocument,
 } from "../../firebase/firebase.utils";
-import { googleSignInSuccess, googleSignInFailure } from "./user.actions";
+import {
+  googleSignInSuccess,
+  googleSignInFailure,
+  emailSignInSuccess,
+  emailSignInFailure,
+} from "./user.actions";
 
 export function* signInWithGoogle() {
   try {
@@ -27,6 +32,24 @@ export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
 
+export function* signInWithEmail({ payload: { email, password } }) {
+  try {
+    const { user } = yield auth.signInWithEmailAndPassword(email, password);
+    //returned "user" object is the same type with the other "user" object returning from signInWithGoogle/signInWithPopup
+    const userRef = yield call(createUserProfileDocument, user);
+    const userSnapshot = yield userRef.get();
+    yield put(
+      emailSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
+    );
+  } catch (error) {
+    put(emailSignInFailure(error));
+  }
+}
+
+export function* onEmailSignInStart() {
+  yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail);
+}
+
 export function* userSagas() {
-  yield all([call(onGoogleSignInStart)]);
+  yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
 }
