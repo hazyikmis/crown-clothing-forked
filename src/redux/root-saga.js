58fb19a9f08e4cb53c66012795ef96e598bf479e
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-06-25 02:51:32 +0200

    when the user signs out then the cart cleared

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -2,8 +2,9 @@ import { all, call } from "redux-saga/effects";
 
 import { fetchCollectionsStart } from "./shop/shop.sagas";
 import { userSagas } from "./user/user.sagas";
+import { cartSagas } from "./cart/cart.sagas";
 
 export default function* rootSaga() {
   //all: run all sagas concurrently
-  yield all([call(fetchCollectionsStart), call(userSagas)]);
+  yield all([call(fetchCollectionsStart), call(userSagas), call(cartSagas)]);
 }
