78d544e203b1b8ad2990d161b833341074c74383
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-07-03 22:41:59 +0200

    after build

---
+++ client/build/precache-manifest.144bb6d4891d1e0e832b78ada5798fdd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d9fe7ff369964ad35633f5f9dcdaf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "561bb1a6ec4a7ff0efda",
+    "url": "/static/css/main.be601951.chunk.css"
+  },
+  {
+    "revision": "92d698b17708d3fa64d7",
+    "url": "/static/js/2.9cfb0e66.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.9cfb0e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f19ad6b9d7531b8d570",
+    "url": "/static/js/3.f6ec1cce.chunk.js"
+  },
+  {
+    "revision": "14c7a4170b5eddf66405",
+    "url": "/static/js/4.94067efb.chunk.js"
+  },
+  {
+    "revision": "3194cb4726281bba5d06",
+    "url": "/static/js/5.9d64209e.chunk.js"
+  },
+  {
+    "revision": "8f25dfc2f1e898f564c6",
+    "url": "/static/js/6.df63f9d6.chunk.js"
+  },
+  {
+    "revision": "e625a1adfd751deb1969",
+    "url": "/static/js/7.48031627.chunk.js"
+  },
+  {
+    "revision": "50e60fe9c749a5009cd7",
+    "url": "/static/js/8.379fb64c.chunk.js"
+  },
+  {
+    "revision": "bc5e1be3b502bd83a2b4",
+    "url": "/static/js/9.78a8f57a.chunk.js"
+  },
+  {
+    "revision": "561bb1a6ec4a7ff0efda",
+    "url": "/static/js/main.6a471d57.chunk.js"
+  },
+  {
+    "revision": "cf4e8eefa9fe4c2cdba3",
+    "url": "/static/js/runtime-main.51a1b525.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
